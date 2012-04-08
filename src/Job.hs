d7fb263a2e8f581b3ff0c924767215c267b45920
Rickard Lindberg <ricli85@gmail.com>
2012-04-08 10:17:28 +0200

    Move things around.

--- src/Job.hs
+++ src/Job.hs
@@ -1,8 +1,30 @@
 module Job where
 
+import Data.IORef
 import System.Process
 
 data Job = Job String [String] (Maybe ProcessHandle)
 
 name :: Job -> String
 name (Job name args _) = name
+
+createJobs :: [Job]
+createJobs = [ Job "ls" [] Nothing
+             , Job "find" ["/home/rick", "README"] Nothing
+             ]
+
+updateJobs :: Maybe FilePath -> [Job] -> IO [Job]
+updateJobs file = mapM updateJob
+    where
+        updateJob (Job name args Nothing) =
+            case file of
+                Just _ -> do
+                    (_, _, _, handle) <- createProcess (proc name args)
+                    return $ Job name args (Just handle)
+                Nothing -> return $ Job name args Nothing
+        updateJob (Job name args (Just h)) = do
+            exitCode <- getProcessExitCode h
+            case exitCode of
+                Nothing -> return $ Job name args (Just h)
+                _       -> return $ Job name args Nothing
+        updateJob job = return job
--- src/Main.hs
+++ src/Main.hs
@@ -16,42 +16,19 @@ main = do
 
 showMainWindow :: IO ()
 showMainWindow = do
-    jobsRef    <- newIORef [Job "ls" [] Nothing,
-                            Job "find" ["/home/rick", "README"] Nothing]
     builder    <- builderFromFile "interface.glade"
     mainWindow <- builderGetObject builder castToWindow "main_window"
     canvas     <- builderGetObject builder castToDrawingArea "canvas"
+
+    jobsRef    <- newIORef createJobs
+    timeoutAdd (widgetQueueDraw canvas >> return True) 10
+    setupNotifications "src" (\a -> updateJobsRef (Just a) jobsRef)
+
     mainWindow `onDestroy` mainQuit
     canvas     `onExpose`  redraw canvas jobsRef
+
     widgetShowAll mainWindow
-    let x = do
-        widgetQueueDraw canvas
-        return True
-    timeoutAdd x 10
-    setupNotifications "src" (\a -> updateJobsRef (Just a) jobsRef)
     return ()
-
-updateJobsRef :: Maybe FilePath -> IORef [Job] -> IO ()
-updateJobsRef changedFile jobsRef = do
-    jobs <- readIORef jobsRef
-    newJobs <- updateJobs changedFile jobs
-    writeIORef jobsRef newJobs
-
-updateJobs :: Maybe FilePath -> [Job] -> IO [Job]
-updateJobs file = mapM updateJob
-    where
-        updateJob (Job name args Nothing) =
-            case file of
-                Just _ -> do
-                    (_, _, _, handle) <- createProcess (proc name args)
-                    return $ Job name args (Just handle)
-                Nothing -> return $ Job name args Nothing
-        updateJob (Job name args (Just h)) = do
-            exitCode <- getProcessExitCode h
-            case exitCode of
-                Nothing -> return $ Job name args (Just h)
-                _       -> return $ Job name args Nothing
-        updateJob job = return job
 
 builderFromFile :: FilePath -> IO Builder
 builderFromFile path = do
@@ -61,9 +38,14 @@ builderFromFile path = do
 
 redraw canvas jobsRef event = do
     updateJobsRef Nothing jobsRef
-
     (w, h) <- widgetGetSize canvas
     drawin <- widgetGetDrawWindow canvas
     jobs <- readIORef jobsRef
     renderWithDrawable drawin (renderScreen jobs (fromIntegral w) (fromIntegral h))
     return True
+
+updateJobsRef :: Maybe FilePath -> IORef [Job] -> IO ()
+updateJobsRef changedFile jobsRef = do
+    jobs <- readIORef jobsRef
+    newJobs <- updateJobs changedFile jobs
+    writeIORef jobsRef newJobs
