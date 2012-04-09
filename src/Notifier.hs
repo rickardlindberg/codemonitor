145120143b6ec447ee898c59d2a2c4361372dd6b
Rickard Lindberg <ricli85@gmail.com>
2012-04-09 09:49:37 +0200

    Rename parameter.

--- src/Notifier.hs
+++ src/Notifier.hs
@@ -3,10 +3,10 @@ module Notifier where
 import System.INotify
 
 setupNotifications :: FilePath -> (String -> IO ()) -> IO ()
-setupNotifications dir nofifyFileChanged = do
+setupNotifications dir notifyFileChanged = do
     i <- initINotify
     addWatch i [Modify, MoveIn, MoveOut] dir handle
     return ()
     where
-        handle (Modified _ (Just f)) = nofifyFileChanged f
+        handle (Modified _ (Just f)) = notifyFileChanged f
         handle _                     = return ()
