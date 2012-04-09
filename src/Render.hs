1fd5979d18551d87206a52d39ff2c58c678cdb5d
Rickard Lindberg <ricli85@gmail.com>
2012-04-09 18:20:30 +0200

    Render failures bigger.

--- src/Render.hs
+++ src/Render.hs
@@ -9,7 +9,7 @@ renderScreen jobs w h = do
     setSourceRGB 1 1 1
     paint
 
-    forM_ (zip jobs (splitVertical (Rect 0 0 w h) (length jobs))) $ \(job, Rect x y w h) -> do
+    forM_ (zip jobs (findRects (Rect 0 0 w h) jobs)) $ \(job, Rect x y w h) -> do
         newPath
         let (r, g, b, a) = color job
         setSourceRGBA r g b a
@@ -17,15 +17,38 @@ renderScreen jobs w h = do
         fill
 
         setSourceRGBA 0 0 0 1
-        moveTo 10 (y+20)
+        moveTo (x + 10) (y+20)
         showText (name job)
 
         let errors = errorText job
         let ys = map (\x -> fromIntegral x*10 + y + 20) [1..length errors]
         forM_ (zip errors ys) $ \(e, y) -> do
-            moveTo 20 y
+            moveTo (x + 20) y
             showText e
         return ()
+
+findRects :: Rect -> [Job] -> [Rect]
+findRects rect jobs =
+    if any isFailed jobs then
+        let (top, bottom) = divideVertical rect 0.9
+            tops = splitVertical top (count isFailed jobs)
+            bottoms = splitHorizontal bottom (count (not . isFailed) jobs)
+        in match tops bottoms jobs
+    else
+        splitVertical rect (length jobs)
+
+match [] [] [] = []
+match tops bottoms (j:js) =
+    if isFailed j then
+        head tops:match (tail tops) bottoms js
+    else
+        head bottoms:match tops (tail bottoms) js
+
+count x y = length (filter x y)
+
+isFailed :: Job -> Bool
+isFailed (Job { status = Fail _ }) = True
+isFailed _ = False
 
 color :: Job -> (Double, Double, Double, Double)
 color (Job { status = Idle }) = (0, 1, 0, 1)
@@ -43,3 +66,13 @@ splitVertical (Rect x y w h) n = map calcNew [1..n]
     where
         calcNew n = Rect x ((fromIntegral(n)-1)*newH) w newH
         newH = h / fromIntegral(n)
+
+splitHorizontal :: Rect -> Int -> [Rect]
+splitHorizontal (Rect x y w h) n = map calcNew [1..n]
+    where
+        calcNew n = Rect ((fromIntegral(n)-1)*newW) y newW h
+        newW = w / fromIntegral(n)
+
+divideVertical :: Rect -> Double -> (Rect, Rect)
+divideVertical (Rect x y w h) percent = (Rect x y w splitH, Rect x splitH w (h - splitH))
+    where splitH = percent * h
