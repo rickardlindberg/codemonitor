6680cebaacdea2ad15924450259228e191b7e84a
Rickard Lindberg <ricli85@gmail.com>
2012-06-07 15:05:04 +0200

    use Ord data type instead of int

--- src/Render/Layout.hs
+++ src/Render/Layout.hs
@@ -1,13 +1,11 @@
 module Render.Layout where
 
-import Data.List
+import GHC.Exts
 import Job.Types
 import Monitor
 import Render.Rect
 
-highLevel   = 100
-mediumLevel = 50
-lowLevel    = 0
+data AttentionLevel = High | Medium | Low deriving (Eq, Ord)
 
 findRects :: Rect -> [Monitor] -> [(Monitor, Rect)]
 findRects originalRect monitors = zip orderedMonitors rectangles
@@ -30,21 +28,21 @@ findRects originalRect monitors = zip orderedMonitors rectangles
                 = let (_, bottomArea) = divideVertical originalRect 0.7
                       bottoms = splitHorizontal bottomArea numMonitors
                   in bottoms
-        numMonitors       = length orderedMonitors
-        hasHighest        = countLevels highLevel > 0
-        numMedium         = countLevels mediumLevel
-        hasMedium         = numMedium > 0
-        countLevels level = length $ filter ((==level) . attentionLevel) orderedMonitors
+        numMonitors = length orderedMonitors
+        hasHighest  = countAttentionLevels High   orderedMonitors > 0
+        numMedium   = countAttentionLevels Medium orderedMonitors
+        hasMedium   = numMedium > 0
 
 orderMonitors :: [Monitor] -> [Monitor]
-orderMonitors monitors = sortBy cmp monitors
-    where
-        cmp a b
-            | attentionLevel a > attentionLevel b = LT
-            | attentionLevel a < attentionLevel b = GT
-            | otherwise                           = EQ
+orderMonitors = sortWith attentionLevel
 
-attentionLevel :: Monitor -> Int
-attentionLevel (StatusCodeMonitor _ _ _ Fail _) = highLevel
-attentionLevel (StdoutMonitor _ _ _ _)          = mediumLevel
-attentionLevel _                                = lowLevel
+attentionLevel :: Monitor -> AttentionLevel
+attentionLevel (StatusCodeMonitor _ _ _ Fail _) = High
+attentionLevel (StdoutMonitor _ _ _ _)          = Medium
+attentionLevel _                                = Low
+
+countAttentionLevels :: AttentionLevel -> [Monitor] -> Int
+countAttentionLevels level = length . filterAttentionLevel level
+
+filterAttentionLevel :: AttentionLevel -> [Monitor] -> [Monitor]
+filterAttentionLevel level = filter ((==level) . attentionLevel)
