59c05d49f26ee56b895ef61eb76ccb3071fd6c31
Rickard Lindberg <ricli85@gmail.com>
2012-06-18 10:14:50 +0200

    inline readProcessWithExitCode

--- src/Job/Manage.hs
+++ src/Job/Manage.hs
@@ -4,6 +4,7 @@ import Control.Concurrent
 import Control.Exception
 import Job.Types
 import System.Exit
+import System.IO
 import System.Process
 import Text.Regex.Posix
 
@@ -35,10 +36,35 @@ cancel _                        = return ()
 runThread :: Job -> Signaller -> IO ()
 runThread job signalResult = do
     let code = do
-        (exit, stdout, stderr) <- readProcessWithExitCode (name job) (args job) ""
+
+        (Just inh, Just outh, Just errh, pid) <-
+            createProcess (proc (name job) (args job)){ std_in  = CreatePipe,
+                                           std_out = CreatePipe,
+                                           std_err = CreatePipe }
+
+        outMVar <- newEmptyMVar
+
+        -- fork off a thread to start consuming stdout
+        out  <- hGetContents outh
+        _ <- forkIO $ evaluate (length out) >> putMVar outMVar ()
+
+        -- fork off a thread to start consuming stderr
+        err  <- hGetContents errh
+        _ <- forkIO $ evaluate (length err) >> putMVar outMVar ()
+
+        -- wait on the output
+        takeMVar outMVar
+        takeMVar outMVar
+        hClose outh
+        hClose errh
+
+        -- wait on the process
+        exit <- waitForProcess pid
+
         if exit == ExitSuccess
-            then signalResult (jobId job) Idle (stderr ++ stdout)
-            else signalResult (jobId job) Fail (stderr ++ stdout)
+            then signalResult (jobId job) Idle (err ++ out)
+            else signalResult (jobId job) Fail (err ++ out)
+
     onException code (putStrLn "TODO: kill process here")
 
 
