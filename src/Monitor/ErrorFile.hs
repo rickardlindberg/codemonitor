d324130f537a8122d0e65f015474101e99da0c7e
Rickard Lindberg <ricli85@gmail.com>
2012-06-26 18:17:36 +0200

    gracefully handle non existing error file

--- src/Monitor/ErrorFile.hs
+++ src/Monitor/ErrorFile.hs
@@ -1,8 +1,11 @@
 module Monitor.ErrorFile where
 
+import Control.Exception
+import Control.Monad
 import Job.Scheduler
 import Monitor
 import System.Directory
+import System.IO.Error
 
 errorFileName = "codemonitor.errors"
 
@@ -10,7 +13,9 @@ writeErrorFile :: [Monitor] -> IO ()
 writeErrorFile monitors = writeFile errorFileName (errorOutput monitors)
 
 removeErrorFile :: IO ()
-removeErrorFile = removeFile errorFileName
+removeErrorFile = do
+    tryJust (guard . isDoesNotExistError) (removeFile errorFileName)
+    return ()
 
 errorOutput :: [Monitor] -> String
 errorOutput [] = ""
